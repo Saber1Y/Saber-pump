049922961c42a0bb9b258b26f9a00aa6f6dc3eed
Sir...Shades <94114134+Sir-Shaedy@users.noreply.github.com>
2025-02-16 18:42:05 -0500

    Update Token.sol
    The require function was added to validate the _totalSupply input. Additionally, the owner variable was made immutable, and the redundant creator variable was removed, as it served the same purpose as the owner variable.

--- backend/src/Token.sol
+++ backend/src/Token.sol
@@ -4,18 +4,20 @@ pragma solidity 0.8.27;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    address payable public owner;
-    address public creator;
+    address payable public immutable OWNER;
 
     constructor(
-        address _creator,
         string memory _name,
         string memory _symbol,
         uint256 _totalSupply
     ) ERC20(_name, _symbol) {
-        owner = payable(msg.sender);
-        creator = _creator;
-
+        // @param: _name is the name of the token
+        // @param: _symbol is the symbol of the token
+        // @param: _totalSupply is the total supply of the token
+        
+        require(_totalSupply >= 1e18, "Token total supply cannot be 0");
+        
+        OWNER = payable(msg.sender);
         _mint(msg.sender, _totalSupply);
     }
 }
